6922bad9bd5dfd53b3f75f1d36e58f9e08836dad
hp <hp@ShubhamBarge16>
2022-11-18 21:32:19 +0530

    updated first program

--- src/firstProgram/FirstProgram.java
+++ src/firstProgram/FirstProgram.java
@@ -4,7 +4,10 @@ public class FirstProgram
 {
 	public static void main(String[] args) 
 {
-		System.out.println("Welcome Shubham");
-
+			int a= 10;
+			int b= 20;
+			
+			int c = a+b;
+			System.out.println(c);
 }
 }
