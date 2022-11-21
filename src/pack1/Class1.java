25d44fd31a33a9cf68f538c46462ab819fda145a
hp <hp@ShubhamBarge16>
2022-11-21 19:45:19 +0530

    commit on 21.11.22

--- src/pack1/Class1.java
+++ src/pack1/Class1.java
@@ -4,6 +4,21 @@ public class Class1
 {
 public static void main(String[] args) 
 {
-	System.out.println("Hello");
+	int a= 200;
+	int b= 400;
+	int c= a+b;
+	
+	System.out.println(c);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
 }
