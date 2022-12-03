186e08d1c07d5fb56c1348a2e309963d8483ed72
hp <hp@ShubhamBarge16>
2022-12-03 22:39:32 +0530

    Nested if-else-3.12.22

--- src/econditionalStatements/NestedIfElse.java
+++ src/econditionalStatements/NestedIfElse.java
@@ -4,14 +4,26 @@ public class NestedIfElse
 {
 public static void main(String[] args) 
 {
-//Writing if loop within if loop is called as
-//"Nested-if-else statement". 
-//if statement chya aat ajun ek if statement lihaych
-//ani 2 conditions check karaychya. it is called as "Nested if else statement".
-//1st condition true zali tarach 2nd condition check
-//karaychi otherwise loop madhun baher padaych.
+	//EXPLANATION OF "NESTED-IF-ELSE"-->
+//Writing if-else statement within if-else statement is called as
+//"Nested-if-else statement".
+	
+//We are checking 2 conditions in "Nested if-else".
+//****************
+//In this, We are checking 1st condition by writing it in outer "if" statement.
+//If this 1st condition becomes TRUE, then we'll go for checking 2nd condition
+//using one more inner "if-else" statement.
+//But if this 1st condition becomes FALSE, then
+//inner if-else statement will not execute,
+//directly outer else statement will execute. & program will end. 
+	
+//we can execute inner "if-else statement",
+//only if outer "if" condition becomes true.
+//===================================================================================
 	
 //Q-1WAP to check eligibility of a person for Blood Donation.
+//for eg. Assume- Age>=18, Weight>=50.
+	
 //Scenario-1->Age-True, Weight-True.
 	
 //	int age=25;
@@ -34,8 +46,8 @@ public static void main(String[] args)
 //	{
 //		System.out.println("Not Allowed for blood donation due to age criteria");
 //	}
-//	System.out.println("end of program");
-//======================================================================
+//	System.out.println("End of Program");
+//----------------------------------------------------------------------------
 	
 //Scenario-2->Age-True, Weight-False.
 //	int age= 25;
@@ -58,8 +70,8 @@ public static void main(String[] args)
 //	{
 //		System.out.println("not allowed due to age criteria");
 //	}
-//	System.out.println("end of program");
-//==================================================================
+//	System.out.println("End of Program");
+//----------------------------------------------------------------------
 	
 //Scenario-3->Age-False, Weight-true.	
 	
@@ -83,8 +95,8 @@ public static void main(String[] args)
 //	{
 //		System.out.println("not allowed for blood donation due to age criteria");
 //	}
-//	System.out.println("end of program");
-//===================================================================
+//	System.out.println("End of Program");
+//-----------------------------------------------------------------------------
 	
 //Scenario-4->Age-False, Weight-False.
 	
@@ -108,55 +120,109 @@ public static void main(String[] args)
 //	{
 //		System.out.println("Not allowed for blood donation due to age criteria");
 //	}
-//	System.out.println("end of program");
+//	System.out.println("End of Program");
 //======================================================================	
 	
-//Q-WAP to enter a person in mall.
-//Scenario-1- if person has knife=no & ticket=yes.
+//Q-WAP to check eligibility of a person to enter in mall.
+//Scenario-1- if person has knife=yes & ticket=yes.
 	
-	String knife  = "no";
-	String ticket  = "yes";
-	
-	if(knife=="no")
-	{
-		System.out.println("entry in mall");
-		
-		if(ticket=="yes")
-		{
-			System.out.println("can see the movie");
-		}
-		else
-		{
-			System.out.println("can'nt see the movie");
-		}
-	}
-	else
-	{
-		System.out.println("no entry in mall");
-	}
-	System.out.println("end of program");
-//=========================================================================
+//	String knife  = "yes";
+//	String ticket  = "yes";
+//	
+//	if(knife=="yes")
+//	{
+//		System.out.println("can't enter in mall");
+//		
+//		if(ticket=="yes")
+//		{
+//			System.out.println("can't see the movie");
+//		}
+//		else
+//		{
+//			System.out.println("can see the movie");
+//		}
+//	}
+//	else
+//	{
+//		System.out.println("can enter in mall");
+//	}
+//	System.out.println("End of Program");
+//---------------------------------------------------------------------
 	
 //Scenario-2-if person has knife=no & ticket=no.
 	
-	String Knife  = "no";
-	String Ticket  = "no";
-	
-	
-	
-	
-	
-	
-	
-	
+//	String Knife   = "no";
+//	String Ticket  = "no";
+//	
+//	if(Knife=="no")
+//	{
+//		System.out.println("can enter in mall");
+//		
+//		if(Ticket=="no")
+//		{
+//			System.out.println("can't watch the movie");
+//		}
+//		else
+//		{
+//			System.out.println("can watch the movie");
+//		}
+//	}
+//	else
+//	{
+//		System.out.println("can't enter in mall");
+//	}
+//	System.out.println("End of Program");
+//---------------------------------------------------------------------
 
+//Scenario-3-if person has knife=yes & ticket=no.
 	
-
+//	String KNIFE = "yes";
+//	String TICKET = "no";
+//	
+//	if(KNIFE=="yes")
+//	{
+//		System.out.println("Can't enter in mall");
+//		
+//		if(TICKET=="no")
+//		{
+//			System.out.println("can't watch the movie");
+//		}
+//		else
+//		{
+//			System.out.println("can watch the movie");
+//		}
+//	}
+//	else
+//	{
+//		System.out.println("Can enter in mall");
+//	}
+//	System.out.println("End of Program");
+//---------------------------------------------------------------------
 	
+//Scenario-4-if person has knife=no & ticket=yes.
 	
-	
-	
-	
+//	String knife = "no";
+//	String ticket = "yes";
+//	
+//	if(knife=="no")
+//	{
+//		System.out.println("can enter in mall");
+//		
+//		if(ticket=="yes")
+//		{
+//			System.out.println("can watch the movie");
+//		}
+//		else
+//		{
+//			System.out.println("can't watch the movie");
+//		}
+//	}
+//	else
+//	{
+//		System.out.println("can't enter in mall");
+//	}
+//	System.out.println("End of Program");
+//=========================================================================
 	
 	
 
